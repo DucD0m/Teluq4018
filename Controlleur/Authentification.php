a04744c99b3eb9da2f061737902ff8246a4cd645
DucD0m <domupnorth@hotmail.com>
2023-08-25 12:27:34 -0400

    csrf token

--- Controlleur/Authentification.php
+++ Controlleur/Authentification.php
@@ -6,22 +6,29 @@ class Authentification {
 
   public static function get_utilisateur($courriel, $mot_passe) {
     echo "Authentification en cours...\n";
+
     //mysql resquest gestionnaire
     if($mot_passe == 'Gestionnaire') $resultat_gestionnaire = true;
     if($resultat_gestionnaire === true) {
       $_SESSION['auth'] = 'Gestionnaire';
       $utilisateur = new GestionnaireControlleur();
-      return $utilisateur;
     }
+
     //mysql resquest specialiste
     if($mot_passe == 'Specialiste') $resultat_specialiste = true;
     if($resultat_specialiste === true) {
       $_SESSION['auth'] = 'Specialiste';
       $utilisateur = new SpecialisteControlleur();
+    }
+
+    if(get_class($utilisateur) === 'SpecialisteControlleur' || get_class($utilisateur) === 'GestionnaireControlleur'){
+      $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
       return $utilisateur;
     }
-    $self->quitter();
-    echo "Vous n'avez pas accès à cette application.\n";
+    else {
+      echo "Vous n'avez pas accès à cette application.\n";
+      self::quitter();
+    }
   }
 
   public static function quitter() {
--- index.php
+++ index.php
@@ -1,31 +1,38 @@
 <?php
-require_once "Controlleur/ConnexionMySQL.php";
-require_once "Controlleur/Authentification.php";
+
+require "Controlleur/ConnexionMySQL.php";
+require "Controlleur/Authentification.php";
 require_once "Controlleur/GestionnaireControlleur.php";
 require_once "Controlleur/SpecialisteControlleur.php";
 
-//$_POST['courriel'] = "domupnorth@hotmail.com";
-//$_POST['mdp'] = "Gestionnaire";
+$_POST['courriel'] = "domupnorth@hotmail.com";
+$_POST['mdp'] = "Specialiste";
 
-$_SESSION['auth'] = "Specialiste";
+//$_SESSION['auth'] = "Gestionnaire";
 
+// Vérification de session
 if($_SESSION['auth'] === 'Gestionnaire' || $_SESSION['auth'] === 'Specialiste'){
-  $connexion = ConnexionMySQL::connexion();
   if($_SESSION['auth'] === 'Gestionnaire') $utilisateur = new GestionnaireControlleur();
   else if($_SESSION['auth'] === 'Specialiste') $utilisateur = new SpecialisteControlleur();
+  $connexion = ConnexionMySQL::connexion();
   $utilisateur->afficherPage();
 }
+
+// Authentification
 else if(isset($_POST['courriel']) && $_POST['courriel'] != '' && isset($_POST['mdp']) && $_POST['mdp'] != ''){
   $courriel = $_POST['courriel'];
   $mot_passe = $_POST['mdp'];
   $utilisateur = Authentification::get_utilisateur($courriel, $mot_passe);
   if(get_class($utilisateur) === 'SpecialisteControlleur' || get_class($utilisateur) === 'GestionnaireControlleur'){
-
+    $connexion = ConnexionMySQL::connexion();
+    $utilisateur->afficherPage();
   }
-  $utilisateur->afficherPage();
 }
+
+// Affichage de la page d'authentification
 else {
   //require "Vue/Template/auth.php";
   echo "Vous n'êtes pas authentifié.\n";
 }
+
 ?>
