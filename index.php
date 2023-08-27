954faae592bd80ad34fdc157ad647ec75f67044c
DucD0m <domupnorth@hotmail.com>
2023-08-26 21:08:27 -0400

    bug fix fonction quitter 4

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once "Controlleurs/GestionnaireControlleur.php";
 require_once "Controlleurs/SpecialisteControlleur.php";
 
 //if(isset($_POST['quitter']) && $_POST['quitter'] == "oui") {
-  console.log('quitter reçu');
+  echo('quitter reçu');
   Authentification::quitter();
   exit;
 //}
