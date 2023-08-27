73769371a69e026ec172e23f9afc3f5e53c03962
DucD0m <domupnorth@hotmail.com>
2023-08-26 23:24:21 -0400

    Classe PageMenu

--- Controlleurs/GestionnaireControlleur.php
+++ Controlleurs/GestionnaireControlleur.php
@@ -1,16 +1,17 @@
 <?php
+require "Vues/Templates/PageRendezVous.php";
 require_once "Modele/Client.php";
 require_once "Modele/Plan.php";
 require_once "Modele/Notification.php";
 
 class GestionnaireControlleur {
+
   public function afficherPage() {
-    $client = new Client();
-    $client->select_mysql(1);
-    echo $client->get_adhesion()."\n";
-    $plan = new Plan();
-    $plan->select_mysql(1);
-    echo $plan->get_nom()."\n";
+
+    if(!isset($_POST)) {
+      $page = new PageMenu();
+    }
+
   }
 }
 ?>
