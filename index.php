cf68c018c3973d8f1912c826ae8126eb43a6af7e
DucD0m <domupnorth@hotmail.com>
2023-08-26 21:56:57 -0400

    bug fix fonction quitter 14

--- index.php
+++ index.php
@@ -8,13 +8,10 @@ require_once "Controlleurs/SpecialisteControlleur.php";
 var_dump($_POST);
 echo "<br>";
 var_dump($_SESSION);
-echo "<br>";
 
 if(isset($_POST['quitter']) && $_POST['quitter'] == "oui") {
   Authentification::quitter();
 }
-
-echo $_SESSION['auth'];
 
 session_start();
 $connexion = ConnexionBD::connexion();
@@ -31,7 +28,7 @@ if($_SESSION['auth'] === 'Gestionnaire' || $_SESSION['auth'] === 'Specialiste'){
   else if($_SESSION['auth'] === 'Specialiste') $utilisateur = new SpecialisteControlleur();
   $utilisateur->afficherPage();
 }
-echo "apres verif session<br>";
+
 // Authentification
 else if(isset($_POST['auth-courriel']) && $_POST['auth-courriel'] != '' && isset($_POST['auth-mdp']) && $_POST['auth-mdp'] != ''){
   $courriel = $_POST['auth-courriel'];
@@ -41,12 +38,12 @@ else if(isset($_POST['auth-courriel']) && $_POST['auth-courriel'] != '' && isset
     $utilisateur->afficherPage();
   }
 }
-echo "apres verif mot passe<br>";
+
 // Affichage de la page d'authentification
 else {
   //require "Vue/Template/auth.php";
   //echo "Vous n'êtes pas authentifié.\n";
   $page = new PageIndex();
 }
-echo "apres page index<br>";
+
 ?>
