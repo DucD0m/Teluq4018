a29c2e175929d116ecf4fad221a3cb0e7b81396b
DucD0m <domupnorth@hotmail.com>
2023-08-28 07:25:58 -0400

    php ini congig

--- Configuration/config.php
+++ Configuration/config.php
@@ -7,4 +7,6 @@ define("BASEDONNEES", "gym_argente");
 
 define("PEPPER", "9bcf59c3d7751c9d3cdef0c98d32d233146c40819af6df132e28ad7e40c579c1");
 
+ini_set('session.name', 'ab19b0956c3dc02dbca4c1d13ac00cea6b5c7a8f4c9db8ffd99ea5d577063e4a');
+
 ?>
