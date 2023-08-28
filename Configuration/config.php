0e405c8c7295fda64f2e673b21f05b7b477c6a0a
DucD0m <domupnorth@hotmail.com>
2023-08-28 09:15:56 -0400

    php ini config 33

--- Configuration/config.php
+++ Configuration/config.php
@@ -85,7 +85,7 @@ ini_set('session.cookie_lifetime','14400');
 ini_set('session.cookie_httponly','1');
 ini_set('session.cookie_samesite','Strict');
 ini_set('session.cache_expire','30');
-//ini_set('session.sid_length','256');
+ini_set('session.sid_length','256');
 ini_set('session.sid_bits_per_character','6'); //PHP 7.2+
 // session.hash_function            = 1 # PHP 7.0-7.1
 // session.hash_bits_per_character  = 6 # PHP 7.0-7.1
