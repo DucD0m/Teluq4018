381b686fe66d3430c37b07af02ccbcabccb1c280
DucD0m <domupnorth@hotmail.com>
2023-08-28 09:59:13 -0400

    php ini config 42

--- Configuration/config.php
+++ Configuration/config.php
@@ -63,8 +63,8 @@ ini_set('file_uploads','Off');
 
 // PHP session handling
 
-// Ne fonctionne pas. À vérifier ultérieurement...
-//ini_set('session.save_path','/var/www/html/PHP-session/');
+// La configuration par défaut est suffisante pour l'instant.
+ini_set('session.save_path','/var/www/html/PHP-session/');
 
 ini_set('session.name', 'ab19b0956c3dc02dbca4c1d13ac00cea6b5c7a8f4c9db8ffd99ea5d577063e4a');
 //session.auto_start               = Off
