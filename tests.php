d3db8cb8c2c7f2526a97648e697e2b47d1ef6537
DucD0m <domupnorth@hotmail.com>
2023-08-29 12:26:24 -0400

    tests client 22

--- tests.php
+++ tests.php
@@ -5,17 +5,14 @@ ini_set('display_startup_errors','On');
 require_once "Controlleurs/ConnexionLireBD.php";
 require_once "Controlleurs/ConnexionEcrireBD.php";
 require_once "Controlleurs/ConnexionEffacerBD.php";
-//require_once "Tests/ClientTest.php";
+require_once "Tests/ClientTest.php";
 
 
 $connexion_lire = ConnexionLireBD::connexion();
 $connexion_ecrire = ConnexionEcrireBD::connexion();
 $connexion_effacer = ConnexionEffacerBD::connexion();
 
-echo "Test connexions";
-exit;
-
-//$test_client = ClientTest::test_client($connexion_lire,$connexion_ecrire,$connexion_effacer);
+$test_client = ClientTest::test_client($connexion_lire,$connexion_ecrire,$connexion_effacer);
 //echo $test_client;
 
 ?>
