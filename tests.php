f7873c98ce8781c59ec4953f624c42b07e2c904e
DucD0m <domupnorth@hotmail.com>
2023-08-29 12:29:58 -0400

    tests client 27

--- tests.php
+++ tests.php
@@ -11,7 +11,9 @@ $connexion_lire = ConnexionLireBD::connexion();
 $connexion_ecrire = ConnexionEcrireBD::connexion();
 $connexion_effacer = ConnexionEffacerBD::connexion();
 
-$test_client = ClientTest::test_client($connexion_lire,$connexion_ecrire,$connexion_effacer);
-echo $test_client;
+echo "test ici";
+
+//$test_client = ClientTest::test_client($connexion_lire,$connexion_ecrire,$connexion_effacer);
+//echo $test_client;
 
 ?>
