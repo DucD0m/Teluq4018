cafca42da0e1310aed4b7ef446925a10d9a3ff24
DucD0m <domupnorth@hotmail.com>
2023-08-29 12:15:49 -0400

    tests client 18

--- tests.php
+++ tests.php
@@ -3,13 +3,13 @@ ini_set('error_reporting','E_ALL');
 ini_set('display_errors','On');
 ini_set('display_startup_errors','On');
 //require_once "Controlleurs/ConnexionLireBD.php";
-require_once "Controlleurs/ConnexionEcrireBD.php";
+//require_once "Controlleurs/ConnexionEcrireBD.php";
 require_once "Controlleurs/ConnexionEffacerBD.php";
 //require_once "Tests/ClientTest.php";
 
 
 //$connexion_lire = ConnexionLireBD::connexion();
-$connexion_ecrire = ConnexionEcrireBD::connexion();
+//$connexion_ecrire = ConnexionEcrireBD::connexion();
 $connexion_effacer = ConnexionEffacerBD::connexion();
 
 echo "Test connexions";
