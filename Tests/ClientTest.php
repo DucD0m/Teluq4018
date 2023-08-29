df7e46a8162e89bc02ea100cbe337f33b3c5c10f
DucD0m <domupnorth@hotmail.com>
2023-08-29 12:46:06 -0400

    tests client 38

--- Tests/ClientTest.php
+++ Tests/ClientTest.php
@@ -1,10 +1,5 @@
 <?php
-ini_set('error_reporting','E_ALL');
-ini_set('display_errors','On');
-ini_set('display_startup_errors','On');
-
-ksndlcknlsdkc
-//require_once "Modele/Client.php";
+require_once "Modele/Client.php";
 
 // class ClientTest {
 //
