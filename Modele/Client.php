5d1bbedddbafc3b3b5a0c0a2084ed96646538877
DucD0m <domupnorth@hotmail.com>
2023-08-29 08:17:48 -0400

    test strict type attributs 4

--- Modele/Client.php
+++ Modele/Client.php
@@ -4,7 +4,7 @@ require_once "Personne.php";
 class Client extends Personne implements Modele {
 
   protected Int $personne = 0;
-  protected $adhesion = "";
+  protected string $adhesion = "";
   protected $renouvellement = "";
   protected $fin_abonnement = "";
   protected $fin_acces_appareils = "";
