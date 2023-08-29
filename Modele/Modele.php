23b76ed926c5f027444666e9ef63dc54b88f8d24
DucD0m <domupnorth@hotmail.com>
2023-08-29 13:01:35 -0400

    tests client 46

--- Modele/Modele.php
+++ Modele/Modele.php
@@ -1,8 +1,8 @@
-<?php
+<?php declare(strict_types=1);
 interface Modele {
-  public function select_mysql(Int $id, Object $connexion_lire);
-  public function insert_mysql(Object $obj, Object $connexion_ecrire);
-  public function update_mysql(Object $obj, Object $connexion_ecrire);
-  public function delete_mysql(Object $obj, Object $connexion_effacer);
+  public function select_mysql(Int $id, Object $connexion_lire) : Object|Bool;
+  public function insert_mysql(Object $obj, Object $connexion_ecrire) : Int;
+  public function update_mysql(Object $obj, Object $connexion_ecrire) : Object|Bool;
+  public function delete_mysql(Object $obj, Object $connexion_effacer) : Int;
 }
 ?>
