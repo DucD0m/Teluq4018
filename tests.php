95eab2c82fa689fff00dac7c893dedb0af8e9079
DucD0m <domupnorth@hotmail.com>
2023-08-29 12:32:16 -0400

    tests client 29

--- tests.php
+++ tests.php
@@ -1,19 +1,17 @@
 <?php
+ini_set('error_reporting','E_ALL');
+ini_set('display_errors','On');
+ini_set('display_startup_errors','On');
+require_once "Controlleurs/ConnexionLireBD.php";
+require_once "Controlleurs/ConnexionEcrireBD.php";
+require_once "Controlleurs/ConnexionEffacerBD.php";
+require_once "Tests/ClientTest.php";
 
-echo kjnsdckj;
-// ini_set('error_reporting','E_ALL');
-// ini_set('display_errors','On');
-// ini_set('display_startup_errors','On');
-// require_once "Controlleurs/ConnexionLireBD.php";
-// require_once "Controlleurs/ConnexionEcrireBD.php";
-// require_once "Controlleurs/ConnexionEffacerBD.php";
-// require_once "Tests/ClientTest.php";
-//
-// $connexion_lire = ConnexionLireBD::connexion();
-// $connexion_ecrire = ConnexionEcrireBD::connexion();
-// $connexion_effacer = ConnexionEffacerBD::connexion();
-//
-// echo "test ici";
+$connexion_lire = ConnexionLireBD::connexion();
+$connexion_ecrire = ConnexionEcrireBD::connexion();
+$connexion_effacer = ConnexionEffacerBD::connexion();
+
+echo "test ici";
 
 //$test_client = ClientTest::test_client($connexion_lire,$connexion_ecrire,$connexion_effacer);
 //echo $test_client;
