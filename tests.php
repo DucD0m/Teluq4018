f4c323a86c6ffc070db405682cc93d5c25ec5d3c
DucD0m <domupnorth@hotmail.com>
2023-08-29 12:08:22 -0400

    tests client 13

--- tests.php
+++ tests.php
@@ -2,14 +2,16 @@
 require_once "Controlleurs/ConnexionLireBD.php";
 require_once "Controlleurs/ConnexionEcrireBD.php";
 require_once "Controlleurs/ConnexionEffacerBD.php";
-require_once "Tests/ClientTest.php";
+//require_once "Tests/ClientTest.php";
 
-echo "Test require";
-exit;
+
 
 $connexion_lire = ConnexionLireBD::connexion();
 $connexion_ecrire = ConnexionEcrireBD::connexion();
 $connexion_effacer = ConnexionEffacerBD::connexion();
+
+echo "Test connexions";
+exit;
 
 //$test_client = ClientTest::test_client($connexion_lire,$connexion_ecrire,$connexion_effacer);
 //echo $test_client;
