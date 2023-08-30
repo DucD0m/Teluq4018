d150ab911bb3c59b1a94d65d616bcfddbff6fab5
DucD0m <domupnorth@hotmail.com>
2023-08-30 07:54:18 -0400

    tests client 63

--- Tests/ClientTest.php
+++ Tests/ClientTest.php
@@ -2,10 +2,6 @@
 ini_set('error_reporting','E_ALL');
 ini_set('display_errors','On');
 ini_set('display_startup_errors','On');
-// require_once "Controlleurs/ConnexionLireBD.php";
-// require_once "Controlleurs/ConnexionEcrireBD.php";
-// require_once "Controlleurs/ConnexionEffacerBD.php";
-
 
 require_once "Modele/Client.php";
 
@@ -92,11 +88,4 @@ class ClientTest {
         }
     }
 }
-echo "fin classe";
-// $connexion_lire = ConnexionLireBD::connexion();
-// $connexion_ecrire = ConnexionEcrireBD::connexion();
-// $connexion_effacer = ConnexionEffacerBD::connexion();
-//
-// $test_client = ClientTest::test_client($connexion_lire,$connexion_ecrire,$connexion_effacer);
-// echo $test_client;
 ?>
