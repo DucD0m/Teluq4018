473f825c88ed3f6c52d9371824b3a7c8fed902dd
DucD0m <domupnorth@hotmail.com>
2023-08-30 10:52:05 -0400

    select client 8

--- Modele/Client.php
+++ Modele/Client.php
@@ -71,12 +71,12 @@ class Client extends Personne implements Modele {
   public function select_personne_mysql(Int $id, Object $connexion_lire) : Object|Bool {
     $resultat = parent::select_mysql($id, $connexion_lire);
     var_dump($this);
-    // $this->set_id($resultat->get_id());
-    // $this->set_prenom($resultat->get_prenom());
-    // $this->set_nom($resultat->get_nom());
-    // $this->set_adresse($resultat->get_adresse());
-    // $this->set_telephone($resultat->get_telephone());
-    // $this->set_courriel($resultat->get_courriel());
+    $this->set_id($resultat->id;
+    $this->set_prenom($resultat->prenom;
+    $this->set_nom($resultat->nom;
+    $this->set_adresse($resultat->adresse;
+    $this->set_telephone($resultat->telephone;
+    $this->set_courriel($resultat->courriel;
   }
   public function select_mysql(Int $id, Object $connexion_lire) : Object|Bool {
     if($id > 0) {
