1410ba1b5f0821cf5cc2a2eabd52217b6b3e2eb7
DucD0m <domupnorth@hotmail.com>
2023-08-30 16:19:10 -0400

    liste plans page client 15

--- Controlleurs/GestionnaireControlleur.php
+++ Controlleurs/GestionnaireControlleur.php
@@ -25,6 +25,8 @@ class GestionnaireControlleur {
         // $client->set_courriel("louistremblay@google.com");
         //$plans = ListePlans::get_liste($connexion_lire);
         $plans = array();
+        phpinfo();
+        exit;
         $page = new PageClient($client, $plans);
       }
 
