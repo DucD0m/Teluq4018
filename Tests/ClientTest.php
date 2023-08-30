daffc6103187682e33c88666dee67deb295806c9
DucD0m <domupnorth@hotmail.com>
2023-08-30 07:36:53 -0400

    tests client 53

--- Tests/ClientTest.php
+++ Tests/ClientTest.php
@@ -92,11 +92,11 @@ class ClientTest {
         }
     }
 }
-
-$connexion_lire = ConnexionLireBD::connexion();
-$connexion_ecrire = ConnexionEcrireBD::connexion();
-$connexion_effacer = ConnexionEffacerBD::connexion();
-
-$test_client = ClientTest::test_client($connexion_lire,$connexion_ecrire,$connexion_effacer);
-echo $test_client;
+echo "fin classe";
+// $connexion_lire = ConnexionLireBD::connexion();
+// $connexion_ecrire = ConnexionEcrireBD::connexion();
+// $connexion_effacer = ConnexionEffacerBD::connexion();
+//
+// $test_client = ClientTest::test_client($connexion_lire,$connexion_ecrire,$connexion_effacer);
+// echo $test_client;
 ?>
