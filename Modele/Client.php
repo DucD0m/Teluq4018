fe6ac385f9c0344890269803d2832188fc33cc59
DucD0m <domupnorth@hotmail.com>
2023-08-30 08:12:00 -0400

    tests client 73

--- Modele/Client.php
+++ Modele/Client.php
@@ -69,9 +69,9 @@ class Client extends Personne implements Modele {
   }
 
   public function select_personne_mysql(Int $id, Object $connexion_lire) : Object|Bool {
-    $resultat = parent::insert_mysql($id, $connexion_lire);
-    var_dump($resultat);
-    return $resultat;
+    // $resultat = parent::insert_mysql($id, $connexion_lire);
+    // return $resultat;
+    echo "test call";
   }
   public function select_mysql(Int $id, Object $connexion_lire) : Object|Bool {
     if($id > 0) {
