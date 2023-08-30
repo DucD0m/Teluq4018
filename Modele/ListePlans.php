f29f8ef200c0ed69ce17fbe2f305c90132cf70a6
DucD0m <domupnorth@hotmail.com>
2023-08-30 16:06:38 -0400

    liste plans page client 9

--- Modele/ListePlans.php
+++ Modele/ListePlans.php
@@ -19,7 +19,7 @@ class ListePlans {
     //   $plan->set_acces_cours_groupe($resultat->acces_cours_groupe);
     //   $plan->set_prix_cours_groupe($resultat->prix_cours_groupe);
     //   array_push($liste, $plan);
-    self::$liste = ('fraise','bannanes');
+    self::$liste = array('fraise','bannanes');
     }
     return self::$liste;
   }
