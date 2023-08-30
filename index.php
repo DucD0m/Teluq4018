248a66b7a621d25d14c3bbcad47998f66aa7043a
DucD0m <domupnorth@hotmail.com>
2023-08-30 10:16:31 -0400

    tests app 13

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ require_once "Controlleurs/ConnexionLireBD.php";
 require_once "Controlleurs/ConnexionEcrireBD.php";
 require_once "Controlleurs/ConnexionEffacerBD.php";
 require_once "Controlleurs/Authentification.php";
-require_once "Controlleurs/GestionnaireControlleur.php";
 echo "ici"; exit;
+require_once "Controlleurs/GestionnaireControlleur.php";
 require_once "Controlleurs/SpecialisteControlleur.php";
 require_once "Controlleurs/fonctions_php.php";
 
