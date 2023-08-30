c0eda59132323ab99c189a1129f10b88ababbc3d
DucD0m <domupnorth@hotmail.com>
2023-08-30 09:27:58 -0400

    tests client 114

--- Modele/Personne.php
+++ Modele/Personne.php
@@ -92,11 +92,10 @@ abstract class Personne implements Modele {
   }
   public function delete_mysql(Object $obj, Object $connexion_effacer) : Int|Bool {
     if((get_class($obj) === 'Client' || get_class($obj) === 'Gestionnaire' || get_class($obj) === 'Specialiste') && $obj->get_id() > 0) {
-      // $sql = $connexion_effacer->prepare("DELETE FROM personnes WHERE id = :id");
-      // $sql->bindParam(':id', $obj->get_id(), PDO::PARAM_INT);
-      // $sql->execute();
-      // return $sql;
-      return true;
+      $sql = $connexion_effacer->prepare("DELETE FROM personnes WHERE id = :id");
+      $sql->bindParam(':id', $obj->get_id(), PDO::PARAM_INT);
+      $resultat = $sql->execute();
+      return $resultat;
     }
     else{
       return false;
