9079ca83a8784d3363af2e6cc4ad6f98a44a78e7
DucD0m <domupnorth@hotmail.com>
2023-08-30 16:40:01 -0400

    liste plans page client 31

--- test.php
+++ test.php
@@ -10,18 +10,17 @@ class ListePlans {
     $sql = $connexion_lire->prepare("SELECT * FROM plans");
     $sql->execute();
     $resultats = $sql->fetchAll(PDO::FETCH_OBJ);
-    self::$liste = $resultats;
-    // foreach ($resultats as $resultat) {
-    //   $plan = new Plan();
-    //   $plan->set_id($resultat->id);
-    //   $plan->set_nom($resultat->nom);
-    //   $plan->set_duree($resultat->duree);
-    //   $plan->set_prix($resultat->prix);
-    //   $plan->set_acces_appareils($resultat->acces_appareils);
-    //   $plan->set_acces_cours_groupe($resultat->acces_cours_groupe);
-    //   $plan->set_prix_cours_groupe($resultat->prix_cours_groupe);
-    //   array_push($liste, $plan);
-    // }
+    foreach ($resultats as $resultat) {
+      $plan = new Plan();
+      $plan->set_id($resultat->id);
+      // $plan->set_nom($resultat->nom);
+      // $plan->set_duree($resultat->duree);
+      // $plan->set_prix($resultat->prix);
+      // $plan->set_acces_appareils($resultat->acces_appareils);
+      // $plan->set_acces_cours_groupe($resultat->acces_cours_groupe);
+      // $plan->set_prix_cours_groupe($resultat->prix_cours_groupe);
+      array_push($liste, $plan);
+    }
     return self::$liste;
   }
 
