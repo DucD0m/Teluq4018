73783243be017a5ea9c4a04f1518f63456c578d5
DucD0m <domupnorth@hotmail.com>
2023-09-02 19:50:15 -0400

    gestion plans 12

--- Vues/Templates/PageClient.php
+++ Vues/Templates/PageClient.php
@@ -32,7 +32,7 @@ class PageClient {
       <meta charset="UTF-8">
 
       <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.1/themes/cupertino/jquery-ui.css">
-      <link rel="stylesheet" href="../css/global.css">
+      <link rel="stylesheet" href="Vues/css/global.css">
 
       <script
         src="https://code.jquery.com/jquery-3.7.0.min.js"
