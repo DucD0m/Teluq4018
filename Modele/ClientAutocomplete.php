0a480fc494875613da773b191bc2a91489d294ce
DucD0m <domupnorth@hotmail.com>
2023-09-03 10:40:05 -0400

    client autocomplete 36

--- Modele/ClientAutocomplete.php
+++ Modele/ClientAutocomplete.php
@@ -1,21 +1,21 @@
 <?php
 require_once __DIR__ . '/../Configuration/config.php';
-//require_once __DIR__ . "/../Controlleurs/ConnexionLireBD.php";
+require_once __DIR__ . "/../Controlleurs/ConnexionLireBD.php";
 
-//$connexion_lire = ConnexionLireBD::connexion();
+$connexion_lire = ConnexionLireBD::connexion();
 
-$serveur = SERVERNAME;
-$lecteur = LECTEUR;
-$mdp_lecteur = MDPLECTEUR;
-$base_donnees = BASEDONNEES;
-
-try {
-  $connexion_lire = new PDO("mysql:host=$serveur;dbname=$base_donnees", $lecteur, $mdp_lecteur);
-  $connexion_lire->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-  //die("Connection failed: " . $e->getMessage());
-  die("Impossible de se connecter en ce moment. Veuillez essayer plus tard.(1)");
-}
+// $serveur = SERVERNAME;
+// $lecteur = LECTEUR;
+// $mdp_lecteur = MDPLECTEUR;
+// $base_donnees = BASEDONNEES;
+//
+// try {
+//   $connexion_lire = new PDO("mysql:host=$serveur;dbname=$base_donnees", $lecteur, $mdp_lecteur);
+//   $connexion_lire->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch(PDOException $e) {
+//   //die("Connection failed: " . $e->getMessage());
+//   die("Impossible de se connecter en ce moment. Veuillez essayer plus tard.(1)");
+// }
 
 $choix_liste = array();
 
