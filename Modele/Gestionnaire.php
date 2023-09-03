9a571c581bdc8fcdeae513966d7b4bec28ee8aad
DucD0m <domupnorth@hotmail.com>
2023-09-03 08:14:22 -0400

    nom gestionnaire 3

--- Modele/Gestionnaire.php
+++ Modele/Gestionnaire.php
@@ -33,13 +33,13 @@ class Gestionnaire extends Personne implements Modele {
   public function select_mysql(Int $id, Object $connexion_lire) : Object|Bool {
     // Code here
   }
-  public function insert_mysql(Object $obj, Object $connexion_ecrire) Int|Bool {
+  public function insert_mysql(Object $obj, Object $connexion_ecrire) : Int|Bool {
     // Code here
   }
-  public function update_mysql(Object $obj, Object $connexion_ecrire) Int|Bool {
+  public function update_mysql(Object $obj, Object $connexion_ecrire) : Int|Bool {
     // Code here
   }
-  public function delete_mysql(Object $obj, Object $connexion_effacer) Int|Bool {
+  public function delete_mysql(Object $obj, Object $connexion_effacer) : Int|Bool {
     // Code here
   }
 }
