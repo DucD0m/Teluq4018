7e9e73d3c39a686525612cbb4049294a1dfd35a5
DucD0m <domupnorth@hotmail.com>
2023-09-03 10:46:06 -0400

    client autocomplete 39

--- Vues/Templates/PageMenu.php
+++ Vues/Templates/PageMenu.php
@@ -557,7 +557,7 @@ class PageMenu {
                  "ui-tooltip": "ui-corner-all"
                },
                position: {
-                 my: "left top-30", at: "left bottom", collision: "flipfit" 
+                 my: "left top-100", at: "left bottom", collision: "flipfit" 
                }
              });
           });
