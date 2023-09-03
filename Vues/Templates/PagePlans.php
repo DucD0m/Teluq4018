f2c39aeb755781c67cbeea75beb03644b11ae1da
DucD0m <domupnorth@hotmail.com>
2023-09-02 21:47:43 -0400

    gestion plans 43

--- Vues/Templates/PagePlans.php
+++ Vues/Templates/PagePlans.php
@@ -471,12 +471,6 @@ class PagePlans {
 
     <body>
 
-        <?php if(isset($_SESSION['message']) && ($_SESSION['message'] != '')): ?>
-          <script>
-            alert("<?php echo $_SESSION['message']; ?>");
-          </script>
-        <?php unset($_SESSION['message']); endif; ?>
-
         <form id="retour-form" class="hidden" action="http://10.0.1.18" method="post">
           <input type="hidden" id="retour-input" name="retour" value="oui">
           <button id="retour-menu" class="couleurs retour" type="submit" value="submit">
@@ -678,6 +672,14 @@ class PagePlans {
             });
           });
         </script>
+
+        <?php if(isset($_SESSION['message']) && ($_SESSION['message'] != '')): ?>
+          <script>
+          $( document ).ready(function() {
+            alert("<?php echo $_SESSION['message']; ?>");
+          });
+          </script>
+        <?php unset($_SESSION['message']); endif; ?>
 
     </body>
     </html>
