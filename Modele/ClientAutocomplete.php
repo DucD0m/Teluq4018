e812307550368e69b456d5ed321f901f76520171
DucD0m <domupnorth@hotmail.com>
2023-09-03 09:31:01 -0400

    client autocomplete 21

--- Modele/ClientAutocomplete.php
+++ Modele/ClientAutocomplete.php
@@ -17,28 +17,28 @@ try {
   die("Impossible de se connecter en ce moment. Veuillez essayer plus tard.(1)");
 }
 
-//$choix_liste = array();
 $choix_liste = array();
-array_push($choix_liste,$_GET['term']);
+//$choix_liste = array();
+//array_push($choix_liste,$_GET['term']);
 
 //$_GET["term"] = "Duc";
 
-// $client = trim($_GET["term"]);
-//
-// if($client != ""){
-//   $sql = $connexion_lire->prepare("SELECT id, prenom, nom, telephone FROM personnes WHERE prenom LIKE '%$client%' OR nom LIKE '%$client%' OR telephone LIKE '%$client%' ORDER BY prenom ASC");
-// 	// $sql = $connexion_lire->prepare("SELECT id, prenom, nom, telephone FROM personnes WHERE prenom LIKE CONCAT('%',:prenom,'%') OR nom LIKE CONCAT('%',:nom,'%') OR telephone LIKE CONCAT('%',:tel,'%') ORDER BY prenom ASC");
-//   // $sql->bindParam(':prenom', $client, PDO::PARAM_STR);
-//   // $sql->bindParam(':nom', $client, PDO::PARAM_STR);
-//   // $sql->bindParam(':tel', $client, PDO::PARAM_INT);
-//   $sql->execute();
-//   $resultats = $sql->fetchAll(PDO::FETCH_OBJ);
-//
-//   foreach ($resultats as $resultat) {
-//     $choix = $resultat->id." - ".$resultat->prenom." ".$resultat->nom." - ".$resultat->telephone;
-//     array_push($choix_liste, $choix);
-//   }
+$client = trim($_GET["term"]);
+
+if($client != ""){
+  $sql = $connexion_lire->prepare("SELECT id, prenom, nom, telephone FROM personnes WHERE prenom LIKE '%$client%' OR nom LIKE '%$client%' OR telephone LIKE '%$client%' ORDER BY prenom ASC");
+	// $sql = $connexion_lire->prepare("SELECT id, prenom, nom, telephone FROM personnes WHERE prenom LIKE CONCAT('%',:prenom,'%') OR nom LIKE CONCAT('%',:nom,'%') OR telephone LIKE CONCAT('%',:tel,'%') ORDER BY prenom ASC");
+  // $sql->bindParam(':prenom', $client, PDO::PARAM_STR);
+  // $sql->bindParam(':nom', $client, PDO::PARAM_STR);
+  // $sql->bindParam(':tel', $client, PDO::PARAM_INT);
+  $sql->execute();
+  $resultats = $sql->fetchAll(PDO::FETCH_OBJ);
+
+  foreach ($resultats as $resultat) {
+    $choix = $resultat->id." - ".$resultat->prenom." ".$resultat->nom." - ".$resultat->telephone;
+    array_push($choix_liste, $choix);
+  }
 
 	echo json_encode($choix_liste);
-//}
+}
 ?>
