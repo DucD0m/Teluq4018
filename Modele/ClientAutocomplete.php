21ac8f7d8b721615de9b06e6d1bbd740a688176f
DucD0m <domupnorth@hotmail.com>
2023-09-03 10:36:28 -0400

    client autocomplete 34

--- Modele/ClientAutocomplete.php
+++ Modele/ClientAutocomplete.php
@@ -26,11 +26,11 @@ $choix_liste = array();
 $client = trim($_GET["term"]);
 
 if($client != ""){
-  $sql = $connexion_lire->prepare("SELECT id, prenom, nom, telephone FROM personnes p JOIN clients c ON p.id = c.personne WHERE prenom LIKE '%$client%' OR nom LIKE '%$client%' OR telephone LIKE '%$client%' ORDER BY prenom ASC");
-	// $sql = $connexion_lire->prepare("SELECT id, prenom, nom, telephone FROM personnes WHERE prenom LIKE CONCAT('%',:prenom,'%') OR nom LIKE CONCAT('%',:nom,'%') OR telephone LIKE CONCAT('%',:tel,'%') ORDER BY prenom ASC");
-  // $sql->bindParam(':prenom', $client, PDO::PARAM_STR);
-  // $sql->bindParam(':nom', $client, PDO::PARAM_STR);
-  // $sql->bindParam(':tel', $client, PDO::PARAM_INT);
+  //$sql = $connexion_lire->prepare("SELECT id, prenom, nom, telephone FROM personnes p JOIN clients c ON p.id = c.personne WHERE prenom LIKE '%$client%' OR nom LIKE '%$client%' OR telephone LIKE '%$client%' ORDER BY prenom ASC");
+	$sql = $connexion_lire->prepare("SELECT id, prenom, nom, telephone FROM personnes p JOIN clients c ON p.id = c.personne WHERE prenom LIKE CONCAT('%',:prenom,'%') OR nom LIKE CONCAT('%',:nom,'%') OR telephone LIKE CONCAT('%',:tel,'%') ORDER BY prenom ASC");
+  $sql->bindParam(':prenom', $client, PDO::PARAM_STR);
+  $sql->bindParam(':nom', $client, PDO::PARAM_STR);
+  $sql->bindParam(':tel', $client, PDO::PARAM_INT);
   $sql->execute();
   $resultats = $sql->fetchAll(PDO::FETCH_OBJ);
 
