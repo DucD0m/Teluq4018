bb3b4a686650bff5db69d92462339188299ee9ac
DucD0m <domupnorth@hotmail.com>
2023-09-04 23:30:26 -0400

    supprimer client 3

--- Vues/Templates/PageMenu.php
+++ Vues/Templates/PageMenu.php
@@ -474,6 +474,15 @@ class PageMenu {
     </head>
 
     <body>
+
+        <?php if(isset($_SESSION['message']) && ($_SESSION['message'] != '')): ?>
+          <script>
+          $( document ).ready(function() {
+            alert("<?php echo $_SESSION['message']; ?>");
+          });
+          </script>
+        <?php unset($_SESSION['message']); endif; ?>
+
         <div class='couleurs logo'>
           GY<i class="fa-solid fa-dumbbell"></i><br>
           ARGENTÉ
