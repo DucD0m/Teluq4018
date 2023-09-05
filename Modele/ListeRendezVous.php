e30fffd9427150b055aa3e10678e73ff95f6af61
DucD0m <domupnorth@hotmail.com>
2023-09-05 13:22:04 -0400

    rdv specialiste 49

--- Modele/ListeRendezVous.php
+++ Modele/ListeRendezVous.php
@@ -10,12 +10,14 @@ class ListeRendezVous {
     if((get_class($obj) === 'Client' || get_class($obj) === 'Specialiste') && $obj->get_id() > 0) {
 
       if(get_class($obj) === 'Client') {
+        $client_id = $obj->get_id();
         $sql = $connexion_lire->prepare("SELECT * FROM rendez_vous WHERE client = :client");
-        $sql->bindParam(':client', $obj->get_id(), PDO::PARAM_INT);
+        $sql->bindParam(':client', $client_id, PDO::PARAM_INT);
       }
       else if(get_class($obj) === 'Specialiste') {
+        $specialiste_id = $obj->get_specialiste_id();
         $sql = $connexion_lire->prepare("SELECT * FROM rendez_vous WHERE specialiste = :specialiste");
-        $sql->bindParam(':specialiste', $obj->get_specialiste_id(), PDO::PARAM_INT);
+        $sql->bindParam(':specialiste', $specialiste_id, PDO::PARAM_INT);
       }
 
       $sql->execute();
