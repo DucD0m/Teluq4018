3411c6236ea14fc842127427a0696e7da3cbb8af
DucD0m <domupnorth@hotmail.com>
2023-09-08 17:59:14 -0400

    config php pour google cloud 13

--- Controlleurs/Authentification.php
+++ Controlleurs/Authentification.php
@@ -68,18 +68,18 @@ class Authentification {
 
   public static function quitter() {
 
-    session_start();
+    // session_start();
 
     $_SESSION = array();
-    //
-    // if (ini_get("session.use_cookies")) {
-    //     $params = session_get_cookie_params();
-    //     setcookie(session_name(), '', time() - 42000,
-    //         $params["path"], $params["domain"],
-    //         $params["secure"], $params["httponly"]
-    //     );
-    // }
-    //
+    
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+
     // session_destroy();
 
   }
