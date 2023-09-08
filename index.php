2daeba21265e136d306b76a14b5b6063bf12261e
DucD0m <domupnorth@hotmail.com>
2023-09-08 15:21:38 -0400

    config php pour google cloud 4

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 // Redirection du traffic http vers https.
 if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
-    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header('HTTP/1.1 301 Moved Permanently');
-    header('Location: ' . $location);
-    exit;
+    // $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    // header('HTTP/1.1 301 Moved Permanently');
+    // header('Location: ' . $location);
+    // exit;
+    die('Vous devez utiliser le protocol https.');
 }
 
 require_once "Configuration/config.php";
