a3ab109c121b2f5f05afefae3c00ca244c7079be
DucD0m <domupnorth@hotmail.com>
2023-09-08 15:17:42 -0400

    config php pour google cloud 3

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+// Redirection du traffic http vers https.
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $location);
+    exit;
+}
+
 require_once "Configuration/config.php";
 require_once "Vues/Templates/PageIndex.php";
 require_once "Controlleurs/ConnexionLireBD.php";
