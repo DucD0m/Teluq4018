b0d1c4aaafc19a9aa2eff71df5c7f6f516edbbb0
DucD0m <domupnorth@hotmail.com>
2023-09-08 21:57:34 -0400

    selct heure specialiste styles 4

--- Vues/Templates/PageClient.php
+++ Vues/Templates/PageClient.php
@@ -867,7 +867,7 @@ class PageClient {
               });
 
               // Pour ajuster la couleur du select à l'ouverture de la page dans FireFox.
-              $('#rdv-heure').css('background-color','#FFF');
+              $('#rdv-heure').css('background-color','white');
             });
           </script>
 
