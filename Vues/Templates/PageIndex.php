5d88a175ba849d488ee1e134783bdc9452cda135
DucD0m <domupnorth@hotmail.com>
2023-09-10 09:18:19 -0400

    changer mot passe

--- Vues/Templates/PageIndex.php
+++ Vues/Templates/PageIndex.php
@@ -32,6 +32,8 @@ class PageIndex {
         <form action="<?php echo URL; ?>" method="post">
           <input class="auth auth-courriel" type="text" id="auth-email" name="auth-courriel" placeholder="courriel">
           <input class="auth auth-mdp" type="password" id="auth-mdp" name="auth-mdp" placeholder="mot de passe">
+          <input class="auth auth-mdp-changer" type="password" id="auth-mdp-changer" name="auth-mdp-changer" placeholder="nouveau mot de passe">
+          <input class="auth auth-mdp-confirmer" type="password" id="auth-mdp-confirmer" name="auth-mdp-confirmer" placeholder="confirmer nouveau mot de passe">
           <input id="auth-soumettre" class="auth auth-submit couleurs" type="submit" value="OUVRIR UNE SESSION">
         </form>
 
