1f9aa527dea7b10579ba0c3ac8ce4499ff7bba84
DucD0m <domupnorth@hotmail.com>
2023-09-10 09:26:19 -0400

    changer mot passe 3

--- Vues/Templates/PageIndex.php
+++ Vues/Templates/PageIndex.php
@@ -25,6 +25,11 @@ class PageIndex {
           require "Composantes/logo.html";
         ?>
 
+        <button id="changer-mdp" class="couleurs quitter">
+          CHANGER<br>
+          MOT DE PASSE
+        </button>
+
         <div class="titre">
           SYSTÈME DE GESTION
         </div>
@@ -32,10 +37,18 @@ class PageIndex {
         <form action="<?php echo URL; ?>" method="post">
           <input class="auth auth-courriel" type="text" id="auth-email" name="auth-courriel" placeholder="courriel">
           <input class="auth auth-mdp" type="password" id="auth-mdp" name="auth-mdp" placeholder="mot de passe">
-          <input class="auth auth-mdp-changer" type="password" id="auth-mdp-changer" name="auth-mdp-changer" placeholder="nouveau mot de passe">
-          <input class="auth auth-mdp-confirmer" type="password" id="auth-mdp-confirmer" name="auth-mdp-confirmer" placeholder="confirmer nouveau mot de passe">
+          <input class="auth auth-mdp-changer" type="password" id="auth-mdp-changer" name="auth-mdp-changer" placeholder="nouveau mot de passe" disabled>
+          <input class="auth auth-mdp-confirmer" type="password" id="auth-mdp-confirmer" name="auth-mdp-confirmer" placeholder="confirmer nouveau mot de passe" disabled>
           <input id="auth-soumettre" class="auth auth-submit couleurs" type="submit" value="OUVRIR UNE SESSION">
         </form>
+
+        <script>
+            $( "#changer-mdp" ).click(function(){
+              $('#auth-mdp-changer').css('display','inline-block');
+              $('#auth-mdp-confirmer').css('display','inline-block');
+              $('#auth-soumettre').css('top','45vw');
+            });
+        </script>
 
     </body>
 
