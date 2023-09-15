8abe4c1e2f23455dbb2338e8b1535b3f657f3f0a
DucD0m <domupnorth@hotmail.com>
2023-09-14 21:52:18 -0400

    v1.0.5

--- Vues/Templates/PageIndex.php
+++ Vues/Templates/PageIndex.php
@@ -107,7 +107,7 @@ class PageIndex {
                     		alert('Le nouveau mot de passe doit contenir au moins 8 caractères.');
                     	} else {
                     		if (mot_passe.match(nombre) && mot_passe.match(minuscule) && mot_passe.match(majuscule) && mot_passe.match(caracteres)) {
-                          $('#auth-mdp-confirmer').prop('disabled') === true;
+                          $('#auth-mdp-confirmer').prop('disabled','true');
                           $( "#auth-formulaire" ).submit();
                     		}
                     		else {
